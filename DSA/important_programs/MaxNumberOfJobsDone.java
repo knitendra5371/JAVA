a8e636b9cbede316b9f451491f8a1d8d5b76ad7d
Nitendra kumar <44522667+knitendra5371@users.noreply.github.com>
2021-01-03 18:15:17 +0530

    Add files via upload

---
+++ DSA/important_programs/MaxNumberOfJobsDone.java
@@ -0,0 +1,56 @@
+package Imp_Programs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MaxNumberOfJobsDone {
+	static class Job {
+		public int start;
+		public int end;
+
+		public Job(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}
+	}
+
+	public static int notConflictJob(List<Job> jList, int index) {
+
+		for (int j = index - 1; j >= 0; j--) {
+			if (jList.get(j).end <= jList.get(index).start)
+				return j;
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+
+		List<Job> jList = new ArrayList<>();
+
+		jList.add(new Job(1, 3));
+		jList.add(new Job(3, 5));
+		jList.add(new Job(3, 4));
+		jList.add(new Job(5, 7));
+		jList.add(new Job(7, 8));
+
+		Collections.sort(jList, (o1, o2) -> o1.end - o2.end);
+		int len = jList.size();
+		int[] maxJob = new int[len];
+		maxJob[0] = 1;
+
+		for (int index = 1; index < len; index++) {
+			int i = notConflictJob(jList, index);
+
+			if (i != -1)
+				maxJob[index] = Math.max(maxJob[index - 1], maxJob[i] + 1);
+			else
+				maxJob[index] = Math.max(maxJob[index - 1], 1);
+
+		}
+
+		System.out.println(maxJob[len - 1]);
+
+	}
+
+}
